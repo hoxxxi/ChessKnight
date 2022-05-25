896138bb1d577323e970fbb563bd9f39bd01b67f
HoxXxi <hoxxxii@gmail.com>
2022-05-25 20:24:42 +0100

    simplify solution to remember distance not the whole path and add distance optimisation in move selection

--- src/main/java/ChessKnight.java
+++ src/main/java/ChessKnight.java
@@ -2,19 +2,22 @@ import java.util.LinkedList;
 import java.util.PriorityQueue;
 
 public class ChessKnight {
-    //shortest path from top left to bottom right on a custom rectangular chess board
+    //shortest path from bottom right to top left on a custom rectangular chess board
     //can be easily extended to provide the shortest path to from/to any coordinates on the board
-    public static int solution(int[][] A) {
+    final static Move TARGET = new Move(0, 0, Integer.MAX_VALUE);
+
+    public static int solution(int[][] chessBoard) {
         // if lists are empty exit early
-        if (A.length < 1 || A[0].length < 1) {
+        if (chessBoard.length < 1 || chessBoard[0].length < 1) {
             return -1;
         }
 
         // we start from bottom right
-        int lastY = A.length - 1;
-        int lastX = A[0].length - 1;
+        int lastY = chessBoard.length - 1;
+        int lastX = chessBoard[0].length - 1;
+
         //if unreachable exit early
-        if (A[lastY][lastX] == 1)
+        if (chessBoard[lastY][lastX] == 1 || chessBoard[0][0] == 1)
             return -1;
 
         // if the custom chess board is 1x1
@@ -23,26 +26,15 @@ public class ChessKnight {
 
         // breadth first search where we discover all positions x moves away
         // before we move to x+1 moves away until we reach the first square on the board
-        PriorityQueue<Path> paths = new PriorityQueue<>();
-        LinkedList<Move> possibleMoves = possibleMoves(new Move(lastX, lastY), copyMatrix(A));
-        for (Move move : possibleMoves) {
-            Path path = new Path(copyMatrix(A));
-            path.list.add(move);
-            paths.offer(path);
-        }
-        while (!paths.isEmpty()) {
-            Path currentPath = paths.poll();
-            Move lastMove = currentPath.list.getLast();
-            if (lastMove.X == 0 && lastMove.Y == 0) {
-                return currentPath.depth;
+        chessBoard[lastY][lastX] = 1;
+        PriorityQueue<Move> moves = new PriorityQueue<>();
+        moves.addAll(possibleMoves(new Move(lastX, lastY, 0), chessBoard));
+        while (!moves.isEmpty()) {
+            Move currentMove = moves.poll();
+            if (currentMove.X == TARGET.X && currentMove.Y == TARGET.Y) {
+                return currentMove.moveCount;
             }
-            currentPath.depth++;
-            possibleMoves = possibleMoves(lastMove, currentPath.matrix);
-            for (Move m : possibleMoves) {
-                Path newPath = Path.copyOf(currentPath);
-                newPath.list.add(m);
-                paths.offer(newPath);
-            }
+            moves.addAll(possibleMoves(currentMove, chessBoard));
         }
         return -1;
     }
@@ -56,98 +48,92 @@ public class ChessKnight {
         if (lastMove.X >= xLimit || lastMove.Y >= yLimit || lastMove.X < 0 || lastMove.Y < 0)
             return list;
 
-        chessBoard[lastMove.Y][lastMove.X] = 1;
-
         //two up + one left
         int x = lastMove.X - 1;
         int y = lastMove.Y - 2;
-        if (y >= 0 && x >= 0 && chessBoard[y][x] == 0)
-            list.add(new Move(x, y));
+        if (y >= 0 && x >= 0 && chessBoard[y][x] == 0) {
+            list.add(new Move(x, y, lastMove.moveCount + 1));
+            chessBoard[y][x] = 1;
+        }
 
         //two up + one right
         x = lastMove.X + 1;
         y = lastMove.Y - 2;
-        if (y >= 0 && x < xLimit && chessBoard[y][x] == 0)
-            list.add(new Move(x, y));
+        if (y >= 0 && x < xLimit && chessBoard[y][x] == 0) {
+            list.add(new Move(x, y, lastMove.moveCount + 1));
+            chessBoard[y][x] = 1;
+        }
 
         //one up + two left
         x = lastMove.X - 2;
         y = lastMove.Y - 1;
-        if (y >= 0 && x >= 0 && chessBoard[y][x] == 0)
-            list.add(new Move(x, y));
+        if (y >= 0 && x >= 0 && chessBoard[y][x] == 0) {
+            list.add(new Move(x, y, lastMove.moveCount + 1));
+            chessBoard[y][x] = 1;
+        }
 
         //one up + two right
         x = lastMove.X + 2;
         y = lastMove.Y - 1;
-        if (y >= 0 && x < xLimit && chessBoard[y][x] == 0)
-            list.add(new Move(x, y));
+        if (y >= 0 && x < xLimit && chessBoard[y][x] == 0) {
+            list.add(new Move(x, y, lastMove.moveCount + 1));
+            chessBoard[y][x] = 1;
+        }
 
         //one down + two left
         x = lastMove.X - 2;
         y = lastMove.Y + 1;
-        if (y < yLimit && x >= 0 && chessBoard[y][x] == 0)
-            list.add(new Move(x, y));
+        if (y < yLimit && x >= 0 && chessBoard[y][x] == 0) {
+            list.add(new Move(x, y, lastMove.moveCount + 1));
+            chessBoard[y][x] = 1;
+        }
 
         //one down + two right
         x = lastMove.X + 2;
         y = lastMove.Y + 1;
-        if (y < yLimit && x < xLimit && chessBoard[y][x] == 0)
-            list.add(new Move(x, y));
+        if (y < yLimit && x < xLimit && chessBoard[y][x] == 0) {
+            list.add(new Move(x, y, lastMove.moveCount + 1));
+            chessBoard[y][x] = 1;
+        }
 
         //two down + one left
         x = lastMove.X - 1;
         y = lastMove.Y + 2;
-        if (y < yLimit && x >= 0 && chessBoard[y][x] == 0)
-            list.add(new Move(x, y));
+        if (y < yLimit && x >= 0 && chessBoard[y][x] == 0) {
+            list.add(new Move(x, y, lastMove.moveCount + 1));
+            chessBoard[y][x] = 1;
+        }
 
         //two down + one right
         x = lastMove.X + 1;
         y = lastMove.Y + 2;
-        if (y < yLimit && x < xLimit && chessBoard[y][x] == 0)
-            list.add(new Move(x, y));
+        if (y < yLimit && x < xLimit && chessBoard[y][x] == 0) {
+            list.add(new Move(x, y, lastMove.moveCount + 1));
+            chessBoard[y][x] = 1;
+        }
 
         return list;
     }
 
-    public static int[][] copyMatrix(int[][] matrix) {
-        int[][] newMatrix = new int[matrix.length][];
-        for (int i = 0; i < matrix.length; i++)
-            newMatrix[i] = matrix[i].clone();
-        return newMatrix;
-    }
-
-    private static class Path implements Comparable<Path> {
-        LinkedList<Move> list;
-        int[][] matrix;
-        int depth;
-
-        public Path(int[][] matrix) {
-            this.list = new LinkedList<>();
-            this.matrix = matrix;
-            this.depth = 1;
-        }
-
-        public static Path copyOf(Path path) {
-            Path newPath = new Path(copyMatrix(path.matrix));
-            newPath.depth = path.depth;
-            for (Move move : path.list)
-                newPath.list.add(new Move(move.X, move.Y));
-            return newPath;
-        }
-
-        @Override
-        public int compareTo(Path o) {
-            return Integer.compare(this.depth, o.depth);
-        }
-    }
-
-    private static class Move {
+    private static class Move implements Comparable<Move> {
+        int moveCount;
         int X;
         int Y;
 
-        public Move(int X, int Y) {
+        public Move(int X, int Y, int move) {
+            this.moveCount = move;
             this.X = X;
             this.Y = Y;
+        }
+
+        @Override
+        public int compareTo(Move other) {
+            if (this.moveCount == other.moveCount) {
+                int thisDistanceToTarget = Math.abs(this.X - TARGET.X) + Math.abs(this.Y - TARGET.Y);
+                int otherDistanceToTarget = Math.abs(other.X - TARGET.X) + Math.abs(other.Y - TARGET.Y);
+                return Integer.compare(thisDistanceToTarget, otherDistanceToTarget);
+            }
+            return Integer.compare(this.moveCount, other.moveCount);
         }
     }
 }
--- src/test/java/ChessKnightTest.java
+++ src/test/java/ChessKnightTest.java
@@ -7,14 +7,26 @@ public class ChessKnightTest {
     /**
      * Simple chess board:
      * 000
+     * 000
+     * 000
+     * 000
+     */
+    @Test
+    public void testEmptyChessBoard() {
+        int[][] A = new int[4][3];
+        assertEquals(3, ChessKnight.solution(A));
+    }
+
+    /**
+     * Restricted chess board:
+     * 000
      * 001
      * 100
      * 000
      */
     @Test
-    public void testSimpleChessBoard() {
+    public void testRestrictedChessBoard() {
         int[][] A = new int[4][3];
-        assertEquals(3, ChessKnight.solution(A));
         A[1][2] = 1;
         A[2][0] = 1;
         assertEquals(7, ChessKnight.solution(A));
